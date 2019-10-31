d945fe3a9322b822c9e59e89639724a2a7ef139b
juankmp <juankmp@gmail.com>
2019-10-31 04:52:56 -0500

    Desarrollado hasta el punto 6

--- module-1/lab-list-comprehensions/your-code/main.py
+++ module-1/lab-list-comprehensions/your-code/main.py
@@ -1,23 +1,35 @@
 #Respuesta Juan Munoz
 
+#import numpy as np
 
 #1. Use a list comprehension to create and print a list of consecutive integers starting with 1 and ending with 50.
 
 #Primero el bucle tradicional
-list1 = []
+list11 = []
 for i in range(1,51):
-    list1.append(i)
-print(list1)
+    list11.append(i)
+print(list11)
 
 #Respuesta en list comprehensions
-list1 = [(i+1) for i in range(50)]
-print(list1)
+list12 = [(i+1) for i in range(50)]
+print(list12)
 
 #2. Use a list comprehension to create and print a list of even numbers starting with 2 and ending with 200.
 
+#Primero el bucle tradicional
+list21 = []
+for i in range(1,101):
+    list21.append(i*2)
+print(list21)
+
+#Respuesta en list comprehensions
+list22 = [(i*2) for i in range(101) if i != 0]
+print(list22)
+
+
 #3. Use a list comprehension to create and print a list containing all elements of the 10 x 4 Numpy array below.
 
-a = np.array([[0.84062117, 0.48006452, 0.7876326 , 0.77109654],
+a = ([[0.84062117, 0.48006452, 0.7876326 , 0.77109654],
        [0.44409793, 0.09014516, 0.81835917, 0.87645456],
        [0.7066597 , 0.09610873, 0.41247947, 0.57433389],
        [0.29960807, 0.42315023, 0.34452557, 0.4751035 ],
@@ -28,11 +40,35 @@ a = np.array([[0.84062117, 0.48006452, 0.7876326 , 0.77109654],
        [0.12149553, 0.53222417, 0.9976855 , 0.12536346],
        [0.80930099, 0.50962849, 0.94555126, 0.33364763]])
 
+#Primero el bucle tradicional
+list31 = []
+for i in a:
+    for j in i:
+        list31.append(j)
+print(list31)
+
+#Respuesta en list comprehensions
+list32 = [j for i in a for j in i]
+print(list32)
+
 #4. Add a condition to the list comprehension above so that only values greater than or equal to 0.5 are printed.
+
+#Primero el bucle tradicional
+list41 = []
+for i in a:
+    for j in i:
+        if j >= 0.5:
+            list41.append(j)
+print(list41)
+
+#Respuesta en list comprehensions
+list42 = [j for i in a for j in i if j >= 0.5]
+print(list42)
+
 
 #5. Use a list comprehension to create and print a list containing all elements of the 5 x 2 x 3 Numpy array below.
 
-b = np.array([[[0.55867166, 0.06210792, 0.08147297],
+b = ([[[0.55867166, 0.06210792, 0.08147297],
         [0.82579068, 0.91512478, 0.06833034]],
 
        [[0.05440634, 0.65857693, 0.30296619],
@@ -47,12 +83,39 @@ b = np.array([[[0.55867166, 0.06210792, 0.08147297],
        [[0.07529684, 0.46470767, 0.47984544],
         [0.65368638, 0.14901286, 0.23760688]]])
 
+#Primero el bucle tradicional
+list51 = []
+for i in b:
+    for j in i:
+        for x in j:
+            list51.append(x)
+print(list51)
+
+#Respuesta en list comprehensions
+list52 = [x for i in b for j in i for x in j]
+print(list52)
+
 
 #5. Add a condition to the list comprehension above so that the last value in each subarray is printed, but only if it is less than or equal to 0.5.
+
+#Primero el bucle tradicional
+list61 = []
+for i in b:
+    for j in i:
+        for x in j:
+            if x <= 0.5:
+                list61.append(x)
+print(list61)
+
+#Respuesta en list comprehensions
+list62 = [x for i in b for j in i for x in j if x <= 0.5]
+print(list62)
+
 
 
 #6. Use a list comprehension to select and print the names of all CSV files in the /data directory.
 
+# I do not have the csv files
 
 #7. Use a list comprehension and the Pandas read_csv and concat methods to read all CSV files in the /data directory and combine them into a single data frame. Display the top 10 rows of the resulting data frame.
 
@@ -63,5 +126,3 @@ b = np.array([[[0.55867166, 0.06210792, 0.08147297],
 
 
 #10. Use a list comprehension to extract and print all values from the data set that are between 0.7 and 0.75.¶
-
-main

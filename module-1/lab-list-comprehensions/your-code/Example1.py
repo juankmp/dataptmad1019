e3baa4ac76eb05cbd852cf92737d043b64575dd3
juankmp <juankmp@gmail.com>
2019-10-31 04:01:33 -0500

    Primer Ejercicio

---
+++ module-1/lab-list-comprehensions/your-code/Example1.py
@@ -0,0 +1,15 @@
+
+#import numpy as np
+#import pandas as pd
+
+print('Hola')
+print(range(0,50))
+
+list1 = [(i**2) for i in range(5)]
+print(list1)
+
+list = []
+for i in range(5):
+    list.append(i)
+
+print(list)
